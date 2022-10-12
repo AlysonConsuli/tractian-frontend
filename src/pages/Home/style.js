8720431060f28a3532ff5ab5b9d896bda80b7443
alyson <consulialyson@gmail.com>
2022-10-11 22:42:47 -0300

    fix: logo size

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -8,9 +8,14 @@ export const Logo = styled.div`
   margin: 16px;
   span {
     color: ${({ theme }) => theme.colors.text};
-    font-size: 18px;
+    font-size: 16px;
     font-family: "Saira Stencil One";
     text-align: center;
+  }
+  @media (max-width: ${({ theme }) => theme.breakPoints.mobile}) {
+    span {
+      font-size: 14px;
+    }
   }
 `;
 
